4ca3714b5cff5b8813a2e1b7b99d844d11f027e4
Martinez, AB <ab_martinez@dell.com>
2021-11-02 13:55:45 -0500

    Adds Teacher class.

---
+++ dell-fsd-project-1/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dell_fsd_project_1
+{
+    class Teacher
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public Teacher(){}
+
+        public Teacher(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
