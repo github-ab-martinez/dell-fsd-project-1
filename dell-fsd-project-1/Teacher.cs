70288811d16e6857b4c1c547e3b4e7f1515cf6c3
Martinez, AB <ab_martinez@dell.com>
2021-11-02 14:17:00 -0500

    Updates Teacher class to require id param on instantiation.

--- dell-fsd-project-1/Teacher.cs
+++ dell-fsd-project-1/Teacher.cs
@@ -14,10 +14,11 @@ namespace dell_fsd_project_1
         public string Class { get; private set; }
         public string Section { get; private set; }
 
-        public Teacher(){}
+        public Teacher(int id) { Id = id; }
 
-        public Teacher(string firstName, string lastName, string className, string section)
+        public Teacher(int id, string firstName, string lastName, string className, string section)
         {
+            Id = id;
             FirstName = firstName;
             LastName = lastName;
             Class = className;
